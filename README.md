cd48d10c3e0456ee7b1b483df2e89b8ef94b2384
MitchGoudkuil <mitch_goudkuil@hotmail.com>
2018-10-31 10:03:34 +0100

    Added some questions to the readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Functional programming
+
+#### 5 onderzoeksvragen
+   - Welk genre wordt het meest uitgeleend?
+   - Van welk genre zijn er de meeste boeken?
+   - Wat is de top 10 meest uitgeleende boeken?
+   - Wat is de algemene dikte van de boeken in de oba?
+   -
--- index.js
+++ index.js
@@ -3,16 +3,31 @@ require('dotenv').config()
 const axios = require('axios');
 const parseString = require('xml2js').parseString;
 
-const siteURL = 'https://zoeken.oba.nl/api/v1/';
-const query = 'search/?q=may';
-const end = 'refine=true';
+const queryToString = require('query-string').stringify
 
-axios.get(siteURL + query + '&authorization=' + process.env.PUBLIC, + '&' + end)
+const siteURL = 'https://zoeken.oba.nl/api/v1/';
+
+
+const query = '&librarian=true&q=harry%20potter&refine=true';
+
+
+var searchTerms = {
+  'frabl': '23E3EDD32F360A7'
+}
+
+const endPoint = 'search'
+
+const totalURL = siteURL + endPoint + '/?authorization=' + process.env.PUBLIC + '&' + queryToString(searchTerms)
+
+//const end = '&librarian=true&q=harry%20potter&refine=true';
+
+axios.get(totalURL)
  .then((response) => {
    // handle success
 
 
    parseString(response.data, function (err, result) {
+
    console.log(result);
    });
 
@@ -20,4 +35,5 @@ axios.get(siteURL + query + '&authorization=' + process.env.PUBLIC, + '&' + end)
  .catch(function (error) {
    // handle error
    console.log(error);
+   console.log(totalURL)
  });
